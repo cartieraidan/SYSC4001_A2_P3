a7d5da636ac605c1155ebbe1b9a5ce4114551f2c
cartieraidan <cartieraidan@gmail.com>
2025-11-07 15:55:14 -0500

    Update interrupts.hpp
    example template

--- interrupts.hpp
+++ interrupts.hpp
@@ -1,3 +1,4 @@
+
 #ifndef INTERRUPTS_HPP_
 #define INTERRUPTS_HPP_
 
@@ -9,66 +10,64 @@
 #include<utility>
 #include<sstream>
 #include<iomanip>
-
+#include <algorithm>
 #include<stdio.h>
 
 #define ADDR_BASE   0
 #define VECTOR_SIZE 2
 
-#define CPU_SPEED   100
-#define MEM_LIMIT   1
+struct memory_partition_t {
+    const unsigned int partition_number;
+    const unsigned int size;
+    std::string code;
 
-/**
- * \brief parse the CLI arguments
- *
- * This helper function parses command line arguments and checks for errors 
- * 
- * @param argc number of command line arguments
- * @param argv the command line arguments
- * @return a vector of strings (the parsed vector table)
- * 
- */
-std::tuple<std::vector<std::string>, std::vector<int>> parse_args(int argc, char** argv) {
-    if(argc != 4) {
-        std::cout << "ERROR!\nExpected 3 argument, received " << argc - 1 << std::endl;
-        std::cout << "To run the program, do: ./interrutps <your_trace_file.txt> <your_vector_table.txt> <your_device_table.txt>" << std::endl;
-        exit(1);
-    }
+    memory_partition_t(unsigned int _pn, unsigned int _s, std::string _c):
+        partition_number(_pn), size(_s), code(_c) {}
+};
 
-    std::ifstream input_file;
-    input_file.open(argv[1]);
-    if (!input_file.is_open()) {
-        std::cerr << "Error: Unable to open file: " << argv[1] << std::endl;
-        exit(1);
-    }
+memory_partition_t memory[] = {
+    memory_partition_t(1, 40, "empty"),
+    memory_partition_t(2, 25, "empty"),
+    memory_partition_t(3, 15, "empty"),
+    memory_partition_t(4, 10, "empty"),
+    memory_partition_t(5, 8, "empty"),
+    memory_partition_t(6, 2, "empty")
+};
 
-    std::ifstream input_vector_table;
-    input_vector_table.open(argv[2]);
-    if (!input_vector_table.is_open()) {
-        std::cerr << "Error: Unable to open file: " << argv[2] << std::endl;
-        exit(1);
-    }
+struct PCB{
+    unsigned int    PID;
+    int             PPID;
+    std::string     program_name;
+    unsigned int    size;
+    int             partition_number;
 
-    std::string vector;
-    std::vector<std::string> vectors;
-    while(std::getline(input_vector_table, vector)) {
-        vectors.push_back(vector);
-    }
-    input_vector_table.close();
+    PCB(unsigned int _pid, int _ppid, std::string _pn, unsigned int _size, int _part_num):
+        PID(_pid), PPID(_ppid), program_name(_pn), size(_size), partition_number(_part_num) {}
+};
 
-    std::string duration;
-    std::vector<int> delays;
-    std::ifstream device_table;
-    device_table.open(argv[3]);
-    if (!device_table.is_open()) {
-        std::cerr << "Error: Unable to open file: " << argv[3] << std::endl;
-        exit(1);
-    }
-    while(std::getline(device_table, duration)) {
-        delays.push_back(std::stoi(duration));
-    }
+struct external_file{
+    std::string     program_name;
+    unsigned int    size;
+};
 
-    return {vectors, delays};
+//Allocates a program to memory (if there is space)
+//returns true if the allocation was sucessful, false if not.
+bool allocate_memory(PCB* current) {
+    for(int i = 5; i >= 0; i--) { //Start from smallest partition
+        //check is the code will fit and if the partition is empty
+        if(memory[i].size >= current->size && memory[i].code == "empty") {
+            current->partition_number = memory[i].partition_number;
+            memory[i].code = current->program_name;
+            return true;
+        }
+    }
+    return false;
+}
+
+//frees the memory given PCB.
+void free_memory(PCB* process) {
+    memory[process->partition_number - 1].code = "empty";
+    process->partition_number = -1;
 }
 
 // Following function was taken from stackoverflow; helper function for splitting strings
@@ -86,18 +85,101 @@ std::vector<std::string> split_delim(std::string input, std::string delim) {
     return tokens;
 }
 
-std::tuple<std::string, int> parse_trace(std::string trace) {
+/**
+ * \brief parse the CLI arguments
+ *
+ * This helper function parses command line arguments and checks for errors 
+ * 
+ * @param argc number of command line arguments
+ * @param argv the command line arguments
+ * @return a vector of strings (the parsed vector table), a vector of delays, a vector of external files
+ * 
+ */
+std::tuple<std::vector<std::string>, std::vector<int>, std::vector<external_file>>parse_args(int argc, char** argv) {
+    if(argc != 5) {
+        std::cout << "ERROR!\nExpected 4 argument, received " << argc - 1 << std::endl;
+        std::cout << "To run the program, do: ./interrutps <your_trace_file.txt> <your_vector_table.txt> <your_device_table.txt> <your_external_files.txt>" << std::endl;
+        exit(1);
+    }
+
+    std::ifstream input_file;
+    input_file.open(argv[1]);
+    if (!input_file.is_open()) {
+        std::cerr << "Error: Unable to open file: " << argv[1] << std::endl;
+        exit(1);
+    }
+    input_file.close();
+
+    input_file.open(argv[2]);
+    if (!input_file.is_open()) {
+        std::cerr << "Error: Unable to open file: " << argv[2] << std::endl;
+        exit(1);
+    }
+
+    std::string vector;
+    std::vector<std::string> vectors;
+    while(std::getline(input_file, vector)) {
+        vectors.push_back(vector);
+    }
+    input_file.close();
+
+    std::string duration;
+    std::vector<int> delays;
+    input_file.open(argv[3]);
+
+    if (!input_file.is_open()) {
+        std::cerr << "Error: Unable to open file: " << argv[3] << std::endl;
+        exit(1);
+    }
+
+    while(std::getline(input_file, duration)) {
+        delays.push_back(std::stoi(duration));
+    }
+    input_file.close();
+
+    std::vector<external_file> external_files;
+    input_file.open(argv[4]);
+    if (!input_file.is_open()) {
+        std::cerr << "Error: Unable to open file: " << argv[4] << std::endl;
+        exit(1);
+    }
+
+    std::string file_content;
+    while(std::getline(input_file, file_content)) {
+        external_file entry;
+        auto file_info      = split_delim(file_content, ",");
+
+        entry.program_name  = file_info[0];
+        entry.size          = std::stoi(file_info[1]);
+        external_files.push_back(entry);
+    }
+
+    input_file.close();
+
+
+    return {vectors, delays, external_files};
+}
+
+//Parces each trace and returns a tuple: {Tace activity, duration or interrupt number, program name (if applicable)}
+std::tuple<std::string, int, std::string> parse_trace(std::string trace) {
     //split line by ','
     auto parts = split_delim(trace, ",");
     if (parts.size() < 2) {
         std::cerr << "Error: Malformed input line: " << trace << std::endl;
-        return {"null", -1};
+        return {"null", -1, "null"};
     }
 
     auto activity = parts[0];
     auto duration_intr = std::stoi(parts[1]);
+    std::string extern_file = "null";
 
-    return {activity, duration_intr};
+    auto exec = split_delim(parts[0], " ");
+    if(exec[0] == "EXEC") {
+        extern_file = exec[1];
+        activity = "EXEC";
+    }
+
+    return {activity, duration_intr, extern_file};
 }
 
 //Default interrupt boilerplate
@@ -125,9 +207,9 @@ std::pair<std::string, int> intr_boilerplate(int current_time, int intr_num, int
     return std::make_pair(execution, current_time);
 }
 
-
-void write_output(std::string execution) {
-    std::ofstream output_file("execution.txt");
+//Writes a string to a file
+void write_output(std::string execution, const char* filename) {
+    std::ofstream output_file(filename);
 
     if (output_file.is_open()) {
         output_file << execution;
@@ -139,4 +221,111 @@ void write_output(std::string execution) {
 
     std::cout << "Output generated in execution.txt" << std::endl;
 }
+
+//Helper function for a sanity check. Prints the external files table
+void print_external_files(std::vector<external_file> files) {
+    const int tableWidth = 24;
+
+    std::cout << "List of external files (" << files.size() << " entry(s)): " << std::endl;
+    
+    // Print top border
+    std::cout << "+" << std::setfill('-') << std::setw(tableWidth) << "+" << std::endl;
+    
+    // Print headers
+    std::cout << "|"
+              << std::setfill(' ') << std::setw(10) << "file name"
+              << std::setw(2) << "|"
+              << std::setfill(' ') << std::setw(10) << "files size"
+              << std::setw(2) << "|" << std::endl;
+    
+    // Print separator
+    std::cout << "+" << std::setfill('-') << std::setw(tableWidth) << "+" << std::endl;
+    
+    // Print each PCB entry
+    for (const auto& file : files) {
+        std::cout << "|"
+                  << std::setfill(' ') << std::setw(10) << file.program_name
+                  << std::setw(2) << "|"
+                  << std::setw(10) << file.size
+                  << std::setw(2) << "|" << std::endl;
+    }
+    
+    // Print bottom border
+    std::cout << "+" << std::setfill('-') << std::setw(tableWidth) << "+" << std::endl;
+}
+
+//This function takes as input: the current PCB and the waitqueue (which is a
+//std::vector of the PCB struct); the function returns the information as a table
+std::string print_PCB(PCB current, std::vector<PCB> _PCB) {
+    const int tableWidth = 55;
+
+    std::stringstream buffer;
+    
+    // Print top border
+    buffer << "+" << std::setfill('-') << std::setw(tableWidth) << "+" << std::endl;
+    
+    // Print headers
+    buffer << "|"
+              << std::setfill(' ') << std::setw(4) << "PID"
+              << std::setw(2) << "|"
+              << std::setfill(' ') << std::setw(12) << "program name"
+              << std::setw(2) << "|"
+              << std::setfill(' ') << std::setw(16) << "partition number"
+              << std::setw(2) << "|"
+              << std::setfill(' ') << std::setw(5) << "size"
+              << std::setw(2) << "|" 
+              << std::setfill(' ') << std::setw(8) << "state"
+              << std::setw(2) << "|" << std::endl;
+    
+    // Print separator
+    buffer << "+" << std::setfill('-') << std::setw(tableWidth) << "+" << std::endl;
+
+    buffer << "|"
+                  << std::setfill(' ') << std::setw(4) << current.PID
+                  << std::setw(2) << "|"
+                  << std::setw(12) << current.program_name
+                  << std::setw(2) << "|"
+                  << std::setw(16) << current.partition_number
+                  << std::setw(2) << "|"
+                  << std::setw(5) << current.size
+                  << std::setw(2) << "|"
+                  << std::setw(8) << "running"
+                  << std::setw(2) << "|" << std::endl;
+    
+    // Print each PCB entry
+    for (const auto& program : _PCB) {
+        buffer << "|"
+                  << std::setfill(' ') << std::setw(4) << program.PID
+                  << std::setw(2) << "|"
+                  << std::setw(12) << program.program_name
+                  << std::setw(2) << "|"
+                  << std::setw(16) << program.partition_number
+                  << std::setw(2) << "|"
+                  << std::setw(5) << program.size
+                  << std::setw(2) << "|"
+                  << std::setw(8) << "waiting"
+                  << std::setw(2) << "|" << std::endl;
+    }
+    
+    // Print bottom border
+    buffer << "+" << std::setfill('-') << std::setw(tableWidth) << "+" << std::endl;
+
+    return buffer.str();
+}
+
+
+// Searches the external_files table and returns the size of the program
+unsigned int get_size(std::string name, std::vector<external_file> external_files) {
+    int size = -1;
+
+    for (auto file : external_files) { 
+        if(file.program_name == name){
+            size = file.size;
+            break;
+        }
+    }
+
+    return size;
+}
+
 #endif
